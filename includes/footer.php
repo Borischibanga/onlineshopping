89210898dd2b9c08fa4b7b815a3224f19b15d777
BorisChibanga <malamaborisbwalya@gmail.com>
2023-12-29 09:44:53 +0200

    s

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 Brought to You By <a href="https://quickfindzm.com/">SM TECH in partnership with BBJ RARE TECH</a></strong>
+    </div>
+</footer>
